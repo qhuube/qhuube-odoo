8552521e3d7d93ce852527c49ec13c6926083e32
qhuube <chi@qhuube.com>
2026-03-09 15:22:12 +0000

    Create __manifest__.py

---
+++ qhuube_connector/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "Qhuube",
+    "version": "1.0.0",
+    "summary": "Open Qhuube from Odoo",
+    "category": "Accounting",
+    "depends": ["base"],
+    "data": [
+        "views/qhuube_menu.xml",
+    ],
+    "installable": True,
+    "application": True,
+    "license": "LGPL-3",
+}
